4869a9d894d3d774d28bcb1d8b3afe03f0c8af3b
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-24 16:46:36 -0700

    added code to print the last digit of # in var

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,13 +15,15 @@ int main(void)
 {
 	int n;
 	int last_digit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	last_digit = abs(n) % 10;
+	last_digit = n % 10;
 	if (last_digit < 6 && last_digit != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,
+			       	last_digit);
 	}
 	else if (last_digit < 6 && last_digit == 0)
 	{
