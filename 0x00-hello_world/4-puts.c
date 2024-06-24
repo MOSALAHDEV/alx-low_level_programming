46e636e23fb24e8129e8079b682f922863ee1997
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-24 10:07:58 -0700

    written c program using puts to print a line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 /**
  *main - Entry point 
  *
- *Descripton: A simple program that prints Programming is like building a multilingual puzzle
- *to the standard output using puts function.
+ *Descripton: A simple program that prints a line to the standard output using puts function.
  *
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
