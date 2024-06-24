e8db213e461a474d0c28f647fb8dc3f071d766bb
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-24 15:39:45 -0700

    completed if condition in source code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,29 +7,28 @@
 *main - Entry point
 *
 *Description: generates a random number and determines
-*it is positive negative or zero, 
-*then prints the result to console
+*it is positive negative or zero, then prints the result to console
 *
 *Return: Always 0 (success)
 */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n < 0)
-	{
-	printf("%d is negative\n", n);
-	}
-	else if (n == 0)
-	{
-	printf("%d is zero\n", n);
-	}
-	else 
-	{
-	printf("%d is positive\n", n);
-	}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else 
+{
+printf("%d is positive\n", n);
+}
+return (0);
 }
