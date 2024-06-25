581fd319492595d1fefc9c0039e83c4fb3bfedb7
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-24 19:21:36 -0700

    written program prints alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: print lowercase and uppercase  alphabitical character using while loop
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+
+ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+ch = 'A';
+while (ch <= 'Z')
+{
+putchar (ch);
+ch++;}
+putchar('\n');
+return (0);
+}
