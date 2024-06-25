6c2cacafb61c967f5bd553e31488c3053639a7d3
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-25 16:58:21 -0700

    first project

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,16 @@
 #include "main.h"
+/**
+*main - Entry point
+*Description: prints "_putchar" followed by
+*new line using _putchar function
+*
+*Return: Always 0
+*/
 int main(void)
 {
 	char greet[] = "_putchar\n";
 	int i;
+
 	for (i = 0; greet[i] != '\0'; i++)
 	{
 		_putchar(greet[i]);
