5235a536751d94437dea2ad1ca482c360b66da30
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-25 17:48:25 -0700

    second project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet - print alphabet in lowercase 
+*using _putchar function followed by a newline
+*
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
