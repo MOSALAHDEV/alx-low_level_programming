cd925067b7b458023e6a1533225d32e5dff7e900
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-25 20:39:22 -0700

    7th project done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - compute the absolute value of intger
+ *@n: is the number turned to abs value
+ *
+ *Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return -n;
+	}
+	else
+		return n;
+}
