064e75369d9efc4bff3f99521b7705da34bfb95a
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-27 23:05:27 -0700

    3rd task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
  *_isdigit - checks if a character is a digit or not
  *@c: the character to be checked
  *
- *Return: 1 if a digit otherwise 0 
+ *Return: 1 if a digit otherwise 0
  */
 int _isdigit(int c)
 {
