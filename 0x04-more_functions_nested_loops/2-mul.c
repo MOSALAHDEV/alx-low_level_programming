a319ea4b126e68d1e98d9101f98953309b853574
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-27 23:52:59 -0700

    3rd task done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * mul - multiplies 2 digits
- * @a and @b: digits to be multiplied
- * Return: a*b
- */
+*mul - multiplies 2 digits
+*@a and @b: digits to be multiplied
+*
+*Return: a*b
+*/
 int mul(int a, int b)
 {
 	return (a * b);
