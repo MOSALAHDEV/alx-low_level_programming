9613530bdb2e6148fdea4dc0c50eb8937a77afcd
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-29 15:06:14 -0700

    1st bug done

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void positive_or_negative(int i)
+{
+	int n = 0;
+
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (i == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else
+		printf("%i is positive\n", n);
+}
