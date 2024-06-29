8959fba49ab42f04dda47766652fc1b8ef06f28c
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-29 15:13:40 -0700

    1st bud died

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,14 @@
 #include "main.h"
 void positive_or_negative(int i)
 {
-	int n = 0;
-
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%i is positive\n", n);
+		printf("%i is positive\n", i);
 	}
 	else if (i == 0)
 	{
-		printf("%i is zero\n", n);
+		printf("%i is zero\n", i);
 	}
 	else
-		printf("%i is positive\n", n);
+		printf("%i is positive\n", i);
 }
