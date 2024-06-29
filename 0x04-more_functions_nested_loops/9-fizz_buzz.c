ea5d26f224a9fdac01aecc60966aa601bf47befd
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-29 14:12:15 -0700

    exhausted with FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,23 +15,28 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && num % 5 == 0)
+		if (num % 3 == 0 && !(num % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("Fizz");
 		}
-		else if	(num % 3 == 0)
+		else if	(num % 5 == 0 && !(num % 3 == 0))
 		{
-			printf("fizz ");
+			printf("Buzz");
 		}
-		else if (num % 5 == 0)
+		else if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("buzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d ", num);
 		}
+		if (num != 100)
+		{
+			printf(" ");
+		}
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
