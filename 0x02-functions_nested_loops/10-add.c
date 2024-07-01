cc09bb0d68fc20bc463748123da830dc2aca2185
MOSALAHDEV <mosalah525446@gmail.com>
2024-06-30 20:05:23 -0700

    11th done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,12 @@
  *add - adds two intigers
  *@a: first intiger
  *@b: second intiger
+ *
  *return: sum a + b
  */
 int add(int a, int b)
 {
-	int sum = a+ b;
+	int sum = a + b;
 
 	return (sum);
 }
